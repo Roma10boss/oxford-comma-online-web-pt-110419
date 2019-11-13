542a4a218f123133609421ca50f3d30337ef6ca1
Romario Gustave <romagustave10@gmail.com>
2019-11-13 09:26:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
-  elsif array.length == 1 
+ if array.length == 1 
    return array[0]
+   
   elsif array.length == 2 
    return array.join( "and")
+   
   els array.length > 2 
   return array[0..2].join(",")+ ", and" + array[-1]
 end
