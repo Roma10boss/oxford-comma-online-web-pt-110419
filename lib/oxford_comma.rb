adba59933ed4ff7a13d1dcfe6e7e0b06d927c006
Romario Gustave <romagustave10@gmail.com>
2019-11-13 09:26:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,9 @@
 def oxford_comma(array)
-array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
-return array.join(",")
+  elsif array.length == 1 
+   return array[0]
+  elsif array.length == 2 
+   return array.join( "and")
+  els array.length > 2 
+  return array[0..2].join(",")+ ", and" + array[-1]
+end
 end
